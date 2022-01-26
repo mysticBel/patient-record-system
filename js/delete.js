b06d6c9f9d08c49f777b2927c7f4f62c36c70ca4
mysticBel <dnxbel@gmail.com>
2022-01-26 13:57:48 -0500

    setTimeOut, fadeOut, remove works!

--- js/delete.js
+++ js/delete.js
@@ -1,15 +1,28 @@
 var pacientes = document.querySelectorAll('.paciente');
 console.log(pacientes);
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener('click', function(){
-        console.log('diste click remove')
-        this.remove();
-    });
-});
+var tabla = document.querySelector('#tabla-pacientes');
+tabla.addEventListener('click', function(e){
+    console.log(e.target);
+    e.target.parentNode.classList.add('fadeOut');
+    setTimeout(function(){
+        e.target.parentNode.remove();
+    }, 500);
+    
+    
+})
+
 
 
 // Escucho un evento por cada paciente , hago un loop
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener('click', function(){
+//         console.log('diste click remove')
+//         this.remove();
+//     });
+// });
+
+
 
 
 
