fa1e5aea382e5e385d65cad26d37922565c2dd2c
mysticBel <dnxbel@gmail.com>
2022-01-27 19:53:16 -0500

    getting json data from the server

--- js/search-with-ajax.js
+++ js/search-with-ajax.js
@@ -2,5 +2,9 @@ var btnSearch = document.querySelector('#buscar-paciente');
 btnSearch.addEventListener('click', function(){
     console.log('buscando pacientes...');
     var xhr = new XMLHttpRequest();
-    xhr.open('GET','https://alura-es-cursos.github.io/api-pacientes/pacientes.json')
+    xhr.open('GET','https://alura-es-cursos.github.io/api-pacientes/pacientes.json');
+    xhr.addEventListener('load', function(){
+        console.log(xhr.responseText);
+    })
+    xhr.send();
 });
